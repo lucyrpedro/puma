2d71f8f8fdafb4d51481fc82218465d5bcce5795
lucyrpedro <lu.ufmg@gmail.com>
2020-11-11 21:50:41 +0000

    Updating

--- esdm-test/bin/create-esdm-conf.sh
+++ esdm-test/bin/create-esdm-conf.sh
@@ -1,27 +1,20 @@
 #!/bin/bash
 
-# Script to run xios_test
-
-mkfs.esdm -g -l --create --remove --ignore-errors
-
-if [[ $NUMENS -eq 0 ]] ; then
-  MPIEXEC_ARG="$LAUNCH_MPI_MODEL $MODEL_EXEC"
-else
-  MPIEXEC_ARG=""
-fi
-ens=0
-while [[ $ens -lt $NUMENS ]]; do
-  if [[ $ens -gt 0 ]] ; then
-    MPIEXEC_ARG="$MPIEXEC_ARG : "
-  fi
-  rm -f ${INPUT_DATA_NML}_mem$ens
-  cp $INPUT_DATA_NML ${INPUT_DATA_NML}_mem$ens
-  # Add extra environment variables to ENV_ENS
-  ENV_ENS="env ENS_MEMBER=$ens"
-  MPIEXEC_ARG="$MPIEXEC_ARG $LAUNCH_MPI_MODEL $ENV_ENS $MODEL_EXEC"
-  ((ens=ens+1))
-done
-MPIEXEC_ARG="$MPIEXEC_ARG : $LAUNCH_MPI_XIOS $XIOS_EXEC"
-
-echo "mpiexec $MPIEXEC_ARG"
-mpiexec $MPIEXEC_ARG
+# Create ESDM conf file
+cat > esdm.conf << EOF
+{
+        "esdm": {
+                "backends": [
+                        {
+                                "type": "POSIX",
+                                "id": "p1",
+                                "target": "./_posix1"
+                        }
+                ],
+                "metadata": {
+                        "type": "metadummy",
+                        "id": "md",                     "target": "./_metadummy"
+                }
+        }
+}
+EOF
--- esdm-test/bin/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Script to run
-
-create-xios-test-in.sh;
-create-input-data.sh;
-create-xios-xml.sh;
-create-esdm-conf.sh;
-run-xios-test.sh;
