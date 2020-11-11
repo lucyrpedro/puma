04c04a1151961e532075bcaa93841087b8cc1071
lucyrpedro <lu.ufmg@gmail.com>
2020-11-11 21:25:16 +0000

    Updating xios_test.in

--- esdm-test/bin/create-xios-test-in.sh
+++ esdm-test/bin/create-xios-test-in.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-# Create input grid namelist file
-cat > $INPUT_DATA_NML << EOF
-&input_data
-  filename = '$INPUT_DATA_NC',
-  varname = 'u',
-  varname_xios = 'u',
+# Create input namelist file
+cat > xios_test.in << EOF
+&input
+  do_regrid = .FALSE.,
+  do_ens = .TRUE.,
+  infile_data_base = '$INPUT_DATA_NML',
+  verbose = 1,
+  write_output = .FALSE.,
+  nprocx = $NPROCX,
+  nprocy = $NPROCY,
+  nt = $NTIMES,
+  ncopies = 20
+/
+
+&input_ens
+  num_ens = $NUMENS
 /
 EOF
