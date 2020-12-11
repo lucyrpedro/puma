aeb07fb19a992a09f22d844ca889cb9fe7840d2a
lucyrpedro <lu.ufmg@gmail.com>
2020-12-11 12:39:36 +0000

    Split

---
+++ suite-esdm-xios-split-broadcast/bin/create-esdm-conf.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Create ESDM conf file
+cat > esdm.conf << EOF
+{
+        "esdm": {
+                "backends": [
+                        {
+                                "type": $VAR_TYPE,
+                                "id": $VAR_ID,
+                                "target": $VAR_TARGET
+                        }
+                ],
+                "metadata": {
+                        "type": "metadummy",
+                        "id": "md",
+                        "target": "./_metadummy"
+                }
+        }
+}
+EOF
