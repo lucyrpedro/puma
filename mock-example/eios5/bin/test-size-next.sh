2c4762d8fabad582b630246d4c349ab074b93295
lucyrpedro <lu.ufmg@gmail.com>
2021-01-10 16:56:29 +0000

    eios5 (change the next cycle)

--- mock-example/eios5/bin/test-size-next.sh
+++ mock-example/eios5/bin/test-size-next.sh
@@ -1,40 +1,56 @@
 #!/bin/bash
 
-# BROADCAST=0 => the file is placed in the WORK directory
-# BROADCAST=1 => the file is placed in the SHARE directory
+# The first file is placed in the WORK directory
+
+# BROADCAST=0 => the same file in the next cycle is placed in the WORK directory
+# BROADCAST=1 => the same file in the next cycle is placed in the SHARE directory
         BROADCAST=0
 
+# The location of the previous file is unknown: WORK or SHARE (exactly one) 
+# This location may be saved as a environment variable, if necessary.
+
 # Test if previous file is in the WORK directory and save its size
+
+
         FILE=$CYLC_SUITE_WORK_DIR/$CYLC_TASK_CYCLE_POINT/model/b.txt
+	echo "FILE=$FILE"
+
         if test -f "$FILE"; then
                 SIZE=$(ls -l $FILE | cut -f5 -d " ")
+		echo "SIZE=$SIZE"
         fi
-echo "FILE=$FILE"
 
 # Test if previous file is in the SHARE directory and save its size
+
         FILE=$CYLC_SUITE_SHARE_DIR/$CYLC_TASK_CYCLE_POINT/model/b.txt
+	echo "FILE=$FILE"
+
         if test -f "$FILE"; then
                 SIZE=$(ls -l $FILE | cut -f5 -d " ")
+		echo "SIZE=$SIZE"
         fi
-echo "FILE=$FILE"
 
-# If the size is less than equal to the mean of all files, 
+# If the size is bigger than a THRESHOLD,
 # set BROADCAST to 1 and send the next file to SHARE
 
-#       MEAN=$(< $CYLC_SUITE_RUN_DIR/post/mean.txt)
-        echo "SIZE=$SIZE"
-        MEAN=9000
+        THRESHOLD=5000
 
-        if [[ $SIZE -lt $MEAN ]]; then
+        if [[ $SIZE -gt $THRESHOLD ]]; then
                 BROADCAST=1
         fi
+
+	echo "SIZE=$SIZE"
         echo "BROADCAST=$BROADCAST"
-        NEXT_CYCLE=$(cylc cycle-point --offset-years=1)
-        # echo "NEXT_CYCLE = $NEXT_CYCLE"
+
+#        NEXT_CYCLE=$(cylc cycle-point --offset-years=1)
+#        echo "NEXT_CYCLE = $NEXT_CYCLE"
+
         if [[ $BROADCAST == 1 ]]; then
-            cylc broadcast -n 'model' -s "[environment]VAR_DIR=$CYLC_SUITE_SHARE_DIR/$NEXT_CYCLE/model" "${CYLC_SUITE_NAME}"
-echo "DIR=$CYLC_SUITE_SHARE_DIR/$NEXT_CYCLE/model"
+		cylc broadcast -n 'broadcast-action' -s "[environment]SCRIPT_NAME=move-file-share.sh" "${CYLC_SUITE_NAME}"
         else
-                    cylc broadcast -n 'model' -s "[environment]VAR_DIR=$CYLC_SUITE_WORK_DIR/$NEXT_CYCLE/model" "${CYLC_SUITE_NAME}"
+		cylc broadcast -n 'broadcast-action' -s "[environment]SCRIPT_NAME=move-file-work.sh" "${CYLC_SUITE_NAME}"
         fi
+
+# Here we don't need to specify the cycle because 
+# the conditional is updated in every cycle
 
